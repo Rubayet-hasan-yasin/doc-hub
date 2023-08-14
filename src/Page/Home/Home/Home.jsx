141a4da1d327d691acd6f0e7c432e1abb5b2eb9c
sakib <shakibsorder964@gmail.com>
2023-08-14 18:47:16 +0600

    lka

--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Specialities from "../Specialities/Specialities";
 
 const Home = () => {
 
     return (
         <div>
             <Banner />
+            <Specialities/>
         </div>
     );
 
